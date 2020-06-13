bd4f530551a3dfec4898771c922401ce93c4285e
brandonpham29 <brandonpham29@gmail.com>
2020-06-13 13:35:58 -0600

    Update

--- webscraping/DataNumbers.swift
+++ webscraping/DataNumbers.swift
@@ -31,13 +31,13 @@ class DataNumbers: UIViewController {
         super.viewDidLoad()
     }
     
-    func allInfo(dataArray: [County]) -> [County]{
+    func allInfo() -> [County]{
         do {
             let url = URL(string: "https://www.worldometers.info/coronavirus/usa/texas/" )
             let html = try String(contentsOf: url!, encoding: String.Encoding.ascii)
             let doc:Document = try SwiftSoup.parse(html)
 
-            let x = 10
+            let x = 12
 
             for i in 1...x{
                 var str_county = " ";
--- webscraping/GraphsViewController.swift
+++ webscraping/GraphsViewController.swift
@@ -2,73 +2,63 @@
 //  GraphsViewController.swift
 //  webscraping
 //
-//  Created by Aarish  Brohi on 6/11/20.
+//  Created by Aarish  Brohi on 6/12/20.
 //  Copyright © 2020 Aarish Brohi. All rights reserved.
 //
 
 import UIKit
 import Charts
 
-class GraphsViewController: UIViewController, ChartViewDelegate {
+class GraphsViewController: UIViewController, UITableViewDelegate, ChartViewDelegate, UITableViewDataSource {
+
     
-//    var vc = DataNumbers(nibName: nil, bundle: nil)
+    @IBOutlet weak var tableView: UITableView!
     var data = ViewController(nibName: nil, bundle: nil).data
-  
-    
-    var barChart = BarChartView()
     
     override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         super.viewDidLoad()
-        barChart.delegate = self
-        
-//        data = vc.allInfo(dataArray: data)
-        
-        
         
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+            return 2;
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return tableView.frame.height 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "chartCell") as! GraphsTableViewCell
         
-        var str = Array<String>()
-        
-        
-        barChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.width)
-        barChart.center = view.center
-        
-        var array = [BarChartDataEntry]()
-        
-        view.addSubview(barChart)
-        
-        for i in 0...data.count - 3{
-            
-                array.append(BarChartDataEntry(x: Double(i), y: Double(data[i].cases)))
-                str.append(String(data[i].name))
-            
-     
+        var str = [String]()
+        var array = [Int]()
+        for i in 1...data.count - 1{
+            str.append(String(data[i].name))
         }
         
         
-        let set = BarChartDataSet(entries: array)
-        set.colors = ChartColorTemplates.joyful()
-        let data2 = BarChartData(dataSet: set)
-        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: str)
-        barChart.xAxis.granularity = 1
-        barChart.xAxis.yOffset = 1;
-
+        if indexPath.row == 0 {
+            for i in 1...data.count - 1{
+                array.append(data[i].cases)
+            }
+        }
+            
+        else if indexPath.row == 1 {
+            for i in 1...data.count - 1{
+                array.append(data[i].deaths)
+            }
+        }
         
-        barChart.xAxis.labelPosition = XAxis.LabelPosition.bottomInside
-        barChart.xAxis.avoidFirstLastClippingEnabled = false
-
- 
-        barChart.xAxis.labelFont = UIFont(name: "Verdana", size: 9.0)!
-        barChart.rightAxis.enabled = false
-        barChart.legend.enabled = false
-        barChart.xAxis.drawGridLinesEnabled = false
-
-        barChart.data = data2
-
+        cell.setChart(str: str, values: array)
+        cell.selectionStyle = .none
+        return cell
     }
     
+   
 
 }
